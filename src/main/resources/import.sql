59e730665820bcb079ae477e95474ead55962c99
Synapse-ITA <puggillovalerio@gmail.com>
2024-01-09 20:08:20 +0100

    Imgs added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 INSERT INTO pizza (name, price, description, image) VALUES ('Margherita', 9.99, 'Pizza Margherita classica', '/images/margherita.jpg');
-INSERT INTO pizza (name, price, description, image) VALUES ('Capricciosa', 11.99, 'Pizza Capricciosa speciale', '/images/pepperoni.jpg');
+INSERT INTO pizza (name, price, description, image) VALUES ('Capricciosa', 11.99, 'Pizza Capricciosa speciale', '/images/capricciosa.jpg');
