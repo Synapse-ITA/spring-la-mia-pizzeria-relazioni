ab0102b18b0a2054ba9ea4a499fad8b2560532a5
Synapse-ITA <puggillovalerio@gmail.com>
2024-01-09 13:06:29 +0100

    tasks all mostly done, only missing imgs and html error page

--- src/main/java/org/learning/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.learning.springlamiapizzeriacrud.controller;
 import org.learning.springlamiapizzeriacrud.model.Pizza;
 import org.learning.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizza")
@@ -18,11 +22,21 @@ public class PizzaController {
 
     @GetMapping
     public String index(Model model) {
-        // recupero la lista di libri dal database
         List<Pizza> pizzaList = pizzaRepository.findAll();
-        // aggiungo la lista di libri agli attributi del Model
         model.addAttribute("pizzaList", pizzaList);
         return "pizza/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            Pizza pizza = result.get();
+            model.addAttribute("pizza", pizza);
+            return "pizza/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 
 }
